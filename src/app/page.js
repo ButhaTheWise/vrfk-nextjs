41e9212f5da63c963a613dae3fd72323257f0446
ButhaTheWise <lorincz.gabor.95@gmail.com>
2025-09-03 09:49:53 +0200

    feat: add @iconify-icon/react for icon usage and update layout and styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
-
+import { Icon } from '@iconify-icon/react';
 export default function namePage() {
   return (
-    <div>
-      <h1></h1>
+    <div className="bg-base-200 min-h-screen flex flex-col">
+      <header></header>
+      <main>
+        <div className="alert">
+           <Icon icon="material-symbols:10k" width="12" height="12" />
+           Important notice with alert icon!
+       </div>
+      </main>
+      <footer></footer>
     </div>
   );
 }
