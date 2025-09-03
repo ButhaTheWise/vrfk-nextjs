3352589c4452b0f6630b53f7cdbd87a7e675808e
ButhaTheWise <lorincz.gabor.95@gmail.com>
2025-09-03 10:20:53 +0200

    feat: replace @iconify-icon/react with lucide-react for icon usage in page component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import { Icon } from '@iconify-icon/react';
+import { AArrowDown } from 'lucide-react';
 export default function namePage() {
   return (
     <div className="bg-base-200 min-h-screen flex flex-col">
       <header></header>
       <main>
-        <div className="alert">
-           <Icon icon="material-symbols:10k" width="12" height="12" />
-           Important notice with alert icon!
+        <div>
+           <AArrowDown />Important notice with alert icon!
        </div>
       </main>
       <footer></footer>
